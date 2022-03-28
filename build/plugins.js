d8703ffe2796979fa2cea6cb56aa3deeb8914684
liangpeiqi <liangpeiqi@hoolinks.com>
2022-03-28 23:22:45 +0800

    cli: 打包进度条

--- build/plugins.js
+++ build/plugins.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const WebpackBar = require('webpackbar');
 const { resolveAssetsRootDir } = require('./utils')
 module.exports = [
     // 简化网页打包
@@ -17,5 +18,7 @@ module.exports = [
     // 分离css与js
     new MiniCssExtractPlugin({
         filename: resolveAssetsRootDir('css/[name].css')
-    })
+    }),
+    // 项目打包进度条
+    new WebpackBar(),
 ]
