7caf5cecf2c056ca20927066a46b53f647aee499
liangpeiqi <liangpeiqi@hoolinks.com>
2022-04-10 17:01:25 +0800

    chore: 暂存公共搜索定点组件

--- src/container/views/Home/index.tsx
+++ src/container/views/Home/index.tsx
@@ -1,10 +1,34 @@
-import { ComponentExt } from "@utils/reactExt";
-import React from "react";
+import { AutoSearchBar } from '@components/AutoSearchBar'
+import { ComponentExt } from '@utils/reactExt'
+import { AutoComplete, Input, Rate } from 'antd'
+import React from 'react'
+import { SearchOutlined } from '@ant-design/icons'
 
-export class Home extends ComponentExt {
+interface IState {
+    center: string | BMapGL.Point
+    keyWord: string
+}
+export class Home extends ComponentExt<{}, IState> {
+    state: IState = {
+        center: new BMapGL.Point(116.404449, 39.914889),
+        keyWord: '',
+    }
     render() {
         return (
-            <div>Home</div>
+            <div>
+                <Rate allowHalf defaultValue={2.5} />
+                <AutoSearchBar
+                    onSelect={(v) => {
+                        console.log(v)
+                    }}
+                    placeholder="请输入目标地点"
+                    value={this.state.keyWord}
+                    onChange={(v: string) => {
+                        this.setState({ keyWord: v })
+                    }}
+                ></AutoSearchBar>
+                <SearchOutlined />
+            </div>
         )
     }
 }
