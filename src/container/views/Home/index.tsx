58292ae47cfb0bf8cf2d6c10bd43dd621cc602a8
liangpeiqi <liangpeiqi@hoolinks.com>
2022-04-17 23:07:29 +0800

    chore: 暂存首页代码

--- src/container/views/Home/index.tsx
+++ src/container/views/Home/index.tsx
@@ -1,6 +1,6 @@
 import { AutoSearchBar } from '@components/AutoSearchBar'
 import { ComponentExt } from '@utils/reactExt'
-import { AutoComplete, Input, Rate } from 'antd'
+import { AutoComplete, Input, Rate, Row } from 'antd'
 import React from 'react'
 import { SearchOutlined } from '@ant-design/icons'
 
@@ -16,18 +16,21 @@ export class Home extends ComponentExt<{}, IState> {
     render() {
         return (
             <div>
-                <Rate allowHalf defaultValue={2.5} />
-                <AutoSearchBar
-                    onSelect={(v) => {
-                        console.log(v)
-                    }}
-                    placeholder="请输入目标地点"
-                    value={this.state.keyWord}
-                    onChange={(v: string) => {
-                        this.setState({ keyWord: v })
-                    }}
-                ></AutoSearchBar>
-                <SearchOutlined />
+                <Row align="middle" justify="center">
+                    <AutoSearchBar
+                        style={{width: 300}}
+                        onSelect={(v) => {
+                            console.log(v)
+                        }}
+                        placeholder="请输入目标地点"
+                        value={this.state.keyWord}
+                        onChange={(v: string) => {
+                            console.log(v)
+                            this.setState({ keyWord: v })
+                        }}
+                    ></AutoSearchBar>
+                    <SearchOutlined twoToneColor="#d9d9d9" style={{fontSize: 14, padding: 8, border: '1px solid #d9d9d9', borderLeft: 0}} />
+                </Row>
             </div>
         )
     }
